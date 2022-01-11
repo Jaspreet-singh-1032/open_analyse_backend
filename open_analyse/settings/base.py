8fdcfbb1cb1e8e9602c51f22bfdf29ace8c1d233
Jaspreet-singh-1032 <jassimehra2612@gmail.com>
2022-01-11 21:51:04 +0530

    don't activate heroku settings if code is executed through github actions

--- open_analyse/settings/base.py
+++ open_analyse/settings/base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import django_heroku
 from pathlib import Path
 import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -156,4 +157,7 @@ REST_FRAMEWORK = {
 }
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+if not os.getenv('IS_GITHUB_ACTION'):
+    # if code is not running through github actions
+    # then setup heroku settings
+    django_heroku.settings(locals())
