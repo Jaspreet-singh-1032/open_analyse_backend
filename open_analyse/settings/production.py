06997325156a10a907325a2b86f1637380d24510
Jaspreet-singh-1032 <jassimehra2612@gmail.com>
2022-12-19 21:31:30 +0530

    updated allowed hosts

--- open_analyse/settings/production.py
+++ open_analyse/settings/production.py
@@ -1,7 +1,7 @@
 from .base import *  # noqa
 
 DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1', 'openanalyse.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'openanalyse.pythonanywhere.com']
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 CORS_ALLOWED_ORIGINS = [
     'https://openanalyse.netlify.app'
